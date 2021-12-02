2e272075ec5c7477b5b0bcdefb7cfb94436009ca
juhi-k <juhi.k@elaunchinfotech.in>
2021-12-02 05:53:38 +0000

    role persmition list

--- src/component/pages/role/Create.jsx
+++ src/component/pages/role/Create.jsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import axios from "axios";
 import { useForm } from "react-hook-form";
 //  start custome url define
 import url from "../../../Development.json";
-import { errorResponse, successResponse, isError, configHeaderAxios   } from "../../helpers/response";
-import { Breadcrumb, Form, Check } from 'react-bootstrap';
-import ButtonSubmitReset from '../layouts/ButtonSubmitReset';
+import {
+    errorResponse,
+    successResponse,
+    isError,
+    configHeaderAxios,
+} from "../../helpers/response";
+import { Breadcrumb, Form, Check } from "react-bootstrap";
+import ButtonSubmitReset from "../layouts/ButtonSubmitReset";
 
 const Create = () => {
     let history = useHistory();
     const [id, setId] = useState('');
+    const [roleid, setIdRole] = useState("");
     const [btnloader, setbtnloader] = useState(false);
     const [permissionsGet, setPermissionsGet] = useState([]);
     const [selectPermissions, setSelectPermissions] = useState([]);
-    
+
     const {
         register,
         setValue,
         reset,
         handleSubmit,
-        formState: { errors }
+        formState: { errors },
     } = useForm();
     useEffect(() => {
         PermissionsData();
         let query = new URLSearchParams(history.location.search);
-        let id = query.get('id')
+        let id = query.get("id");
         if (id) {
             fetchData(id);
         }
-    }, [])
+    }, []);
     const PermissionsData = () => {
         const config = configHeaderAxios();
         axios
@@ -44,7 +50,7 @@ const Create = () => {
                     errorResponse(error);
                 }
             });
-    }
+    };
     const fetchData = (id) => {
         let idpass = `?id=${id}`;
         const config = configHeaderAxios();
@@ -52,7 +58,9 @@ const Create = () => {
             .get(process.env.REACT_APP_BASE_URL + url.role_edit + idpass, config)
             .then((response) => {
                 let data = response.data.data;
-                setValue('name', data.name);
+                setValue("name", data.name);
+                // console.log(data.permission_name);
+                // console.log(JSON.parse(data.permission_name));
                 setSelectPermissions(JSON.parse(data.permission_name));
                 setId(data.id);
             })
@@ -61,21 +69,25 @@ const Create = () => {
                     errorResponse(error);
                 }
             });
-    }
+    };
     useEffect(() => {
         isError(errors);
     });
 
     const onSubmit = (data) => {
         setbtnloader(true);
-        data['id'] = id;
+        data["id"] = id;
         const config = configHeaderAxios();
         axios
-            .post(process.env.REACT_APP_BASE_URL + url.role_store, JSON.stringify(data), config)
+            .post(
+                process.env.REACT_APP_BASE_URL + url.role_store,
+                JSON.stringify(data),
+                config
+            )
             .then((response) => {
                 setbtnloader(false);
                 successResponse(response);
-                history.push('/role/list');
+                history.push("/role/list");
             })
             .catch((error) => {
                 setbtnloader(false);
@@ -83,7 +95,7 @@ const Create = () => {
                     errorResponse(error);
                 }
             });
-    }
+    };
 
     return (
         <>
@@ -91,9 +103,9 @@ const Create = () => {
                 <div className="page-heading-left">
                     <Breadcrumb>
                         <Breadcrumb.Item href="/">Admin</Breadcrumb.Item>
-                        <Breadcrumb.Item active>Role {(id) ? "Edit" : "Add"}</Breadcrumb.Item>
+                        <Breadcrumb.Item active>Role {id ? "Edit" : "Add"}</Breadcrumb.Item>
                     </Breadcrumb>
-                    <h1 className="page-header">Role {(id) ? "Edit" : "Add"} </h1>
+                    <h1 className="page-header">Role {id ? "Edit" : "Add"} </h1>
                 </div>
             </div>
             <div className="card">
@@ -102,41 +114,93 @@ const Create = () => {
                         <div className="row">
                             <div className="col-12">
                                 <div className="form-group">
-                                    <label className="form-label" htmlFor="name">Name</label>
+                                    <label className="form-label" htmlFor="name">
+                                        Name
+                                    </label>
 
-                                    <input type="text"
+                                    <input
+                                        type="text"
                                         className="form-control"
                                         id="name"
                                         placeholder="Name"
-                                        {...register('name',  { required: true } )}
+                                        {...register("name", { required: true })}
                                     />
                                 </div>
-                                <div className='checkboxgroup-scroll'>
+                                <div className="checkboxgroup-scroll">
                                     <ul>
-                                        {selectPermissions.length > 0 ?  permissionsGet.map((elem, indx) => {
-                                            // let checkotnot = (selectPermissions.length > 0 && selectPermissions.includes(elem.name)) ? true : false;
-                                            
-                                            return (
-                                                <li className="mb-3" key={indx}>
-                                                    <Form.Check
-                                                        type="checkbox"
-                                                        id={`permission_name_` + indx}
-                                                        label={elem.module}
-                                                        defaultValue={elem.name}
-                                                        defaultChecked={selectPermissions.includes(elem.name) ? true : false}
-                                                        {...register('permission_name', (!id) ? { required: true } : '')}
-                                                    />
-                                                </li>
-                                            )
-                                        }): ''}
-                                        
 
+                                        {selectPermissions.length > 1
+                                            ?
+                                             permissionsGet.map((elem, indx) => {
+                                                return (
+                                                    <li
+                                                        className="mb-3"
+                                                        key={indx}
+                                                        data-permission={selectPermissions.includes(
+                                                            elem.name
+                                                        )}
+                                                    >
+                                                        {console.log(indx)}
+                                                        {console.log(indx)}
+                                                        <div className="custom-control custom-checkbox">
+                                                            <input
+                                                                className="custom-control-input"
+                                                                type="checkbox"
+                                                                name={`permission_name_` + indx}
+                                                                id={`permission_name_` + indx}
+                                                                defaultValue={elem.name}
+                                                                defaultChecked={selectPermissions.includes(elem.name) ? true : false}
+                                                                {...register('permission_name', (!id) ? { required: true } : '')}
 
+                                                            />
+                                                            <label
+                                                                className="custom-control-label"
+                                                                htmlFor={`permission_name_` + indx}
+                                                            >
+                                                                {elem.name}
+                                                            </label>
+                                                        </div>
+                                                    </li>
+                                                );
+                                            })
+                                            :
+
+                                            permissionsGet.map((elem, indx) => {
+                                                return (
+                                                    <li
+                                                        className="mb-3"
+                                                        key={indx}
+                                                        
+                                                    >
+                                                        <div className="custom-control custom-checkbox">
+                                                            <input
+                                                                className="custom-control-input"
+                                                                type="checkbox"
+                                                                name={`permission_name_` + indx}
+                                                                id={`permission_name_` + indx}
+                                                                defaultValue={elem.name}
+                                                                {...register('permission_name', (!id) ? { required: true } : '')}
+
+                                                            />
+                                                            <label
+                                                                className="custom-control-label"
+                                                                htmlFor={`permission_name_` + indx}
+                                                            >
+                                                                {elem.name}
+                                                            </label>
+                                                        </div>
+                                                    </li>
+                                                );
+                                            })
+                                        }
                                     </ul>
                                 </div>
-                                <ButtonSubmitReset btnloader={btnloader} onsubmitFun={() => {
-                                    reset();
-                                }} />
+                                <ButtonSubmitReset
+                                    btnloader={btnloader}
+                                    onsubmitFun={() => {
+                                        reset();
+                                    }}
+                                />
                             </div>
                         </div>
                     </form>
@@ -144,6 +208,6 @@ const Create = () => {
             </div>
         </>
     );
-}
+};
 
 export default Create;
